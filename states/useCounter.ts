1dd24cb531351a563d8d1fcc13f4f85111d2f2c2
Andres <andres.porfirio.tablet@gmail.com>
2023-08-07 15:25:37 -0300

    added persist middleware

--- states/useCounter.ts
+++ states/useCounter.ts
@@ -1,4 +1,6 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 type State = {
   counter: number;
@@ -9,18 +11,26 @@ type Actions = {
   reset: () => void;
 };
 
-const useCounter = create<State & Actions>()((set) => ({
-  counter: 0,
-  add: () => {
-    set((state) => ({
-      counter: state.counter + 1,
-    }));
-  },
-  reset: () => {
-    set(() => ({
+const useCounter = create<State & Actions>()(
+  persist(
+    (set) => ({
       counter: 0,
-    }));
-  },
-}));
+      add: () => {
+        set((state) => ({
+          counter: state.counter + 1,
+        }));
+      },
+      reset: () => {
+        set(() => ({
+          counter: 0,
+        }));
+      },
+    }),
+    {
+      name: "state-useCounter",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
 
 export default useCounter;
